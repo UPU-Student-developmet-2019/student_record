4cbe65cf53d5914bfa1af199062cb956bcbf4b99
R ansh joseph <ranshjosrph@gmail.com>
2021-06-02 10:15:16 +0530

    .env file is added pls dont touch it

--- student_record/settings.py
+++ student_record/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+th8$yy@mp-m5(vrr78k)30)$w0bro(0heq2%#^2e!w@ixrm&c'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
