e16abfb2f4e686f3a754abc32f451d9a12561a44
K GIRISH KUMAR <84152641+GIRISH800@users.noreply.github.com>
2021-06-02 17:43:45 +0530

    OK

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """Run Administrative Tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'student_record.settings')
     try:
         from django.core.management import execute_from_command_line
